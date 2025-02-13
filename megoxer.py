7769234f94fe6318d0db091f318aa1a6670a61c0
Jerryyyyz <mesiop61@gmail.com>
2025-02-13 10:43:38 +0530

    Update megoxer.py

--- megoxer.py
+++ megoxer.py
@@ -8,10 +8,10 @@ from keep_alive import keep_alive
 keep_alive()
 
 # insert your Telegram bot token here
-bot = telebot.TeleBot('TOKEN')
+bot = telebot.TeleBot('7462453713:AAFApXFlCVQed1-slg-lX0NZmpQG62Rjkoc')
 
 # Admin user IDs
-admin_id = ["ADMIN ID"]
+admin_id = ["6295067084"]
 
 # File to store allowed user IDs  
 USER_FILE = "users.txt"
